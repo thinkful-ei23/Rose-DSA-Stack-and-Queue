f0969169999131d69672afdaa837d026c03ce37f
Rose Sorfa <rose.otter.2015@gmail.com>
2018-10-05 13:39:17 -0500

    added is_palindrome function

--- drills.js
+++ drills.js
@@ -24,13 +24,43 @@ function main() {
     }
   }
 
+  //Write an algorithm that uses a stack to determine whether a given input is palindrome or not. 
+  //Use the following template as a starting point.
+  function is_palindrome(string) {
+    string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    //make new stack to hold all letters in the string  
+    const letterStack = new Stack();
+    for(let i = 0; i < string.length; i++) {
+      letterStack.push(string[i]);
+    } 
+    
+    //make new string to hold all letters popped off letterStack 
+    //now the reverse of string is stored in stringReverse
+    let stringReverse = '';
+    while(letterStack.top !== null) {
+      stringReverse = stringReverse + letterStack.pop();
+    }
+    if(string === stringReverse) {
+      return true;
+    }
+    return false;
+  }
+
+  // true, true, true
+  console.log(is_palindrome('dad'));
+  console.log(is_palindrome('A man, a plan, a canal: Panama'));
+  console.log(is_palindrome('1001'));
+  console.log(is_palindrome('Tauhida'));
+
+
   StarTrek.push('Kirk');
   StarTrek.push('Spock');
   StarTrek.push('McCoy');
   StarTrek.push('Scotty');
 
   peek(StarTrek);
-  display(StarTrek);
+  // display(StarTrek);
+  is_palindrome('dad');
   // console.log(peek(StarTrek));
   // console.log(JSON.stringify(StarTrek));
   
